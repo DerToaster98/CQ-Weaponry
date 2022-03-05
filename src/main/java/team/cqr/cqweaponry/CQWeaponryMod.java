765a758b5fae831308d9bde451c5bef809453a35
DerToaster98 <seiler.oli@web.de>
2022-03-05 11:49:26 +0100

    properly configure main class

---
+++ src/main/java/team/cqr/cqweaponry/CQWeaponryMod.java
@@ -0,0 +1,50 @@
+package team.cqr.cqweaponry;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CQWeaponryMod.MODID)
+public class CQWeaponryMod {
+	public static final String MODID = "cqweaponry";
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public CQWeaponryMod() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	// Pre-Init
+	private void setup(final FMLCommonSetupEvent event) {
+	}
+
+	private void doClientStuff(final FMLClientSetupEvent event) {
+		//LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+	}
+
+	// You can use SubscribeEvent and let the Event Bus discover methods to call
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent event) {
+	}
+
+	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
+	// Event bus for receiving Registry Events)
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+		@SubscribeEvent
+		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+			LOGGER.info("HELLO from Register Block");
+		}
+	}
+}
