8e78c02acdae9ea6a7f2cc49207d619a8cdfbfa2
DerToaster98 <seiler.oli@web.de>
2022-03-06 13:46:01 +0100

    add-in armor models

---
+++ src/main/java/team/cqr/cqweaponry/client/model/armor/ModelCrown.java
@@ -0,0 +1,141 @@
+package team.cqr.cqweaponry.client.model.armor;
+
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+
+/**
+ * ModelCrown - DerToaster Created using Tabula 7.0.1
+ */
+public class ModelCrown<T extends LivingEntity> extends ModelCustomArmorBase<T> {
+
+	public ModelRenderer crownBorderFront;
+	public ModelRenderer crownBorderBack;
+	public ModelRenderer crownBorderRight;
+	public ModelRenderer crownBorderLeft;
+	public ModelRenderer crownUpper;
+	public ModelRenderer crownJewelFrontBase;
+	public ModelRenderer crownJewelFront;
+
+	public ModelCrown(float scale) {
+		super(scale, 32, 32);
+
+		this.crownBorderLeft = new ModelRenderer(this, 18, 16);
+		this.crownBorderLeft.setPos(3.0F, 1.3F, 0.0F);
+		this.crownBorderLeft.addBox(0.0F, -9.0F, -3.0F, 1, 2, 6, 0.0F);
+		this.crownJewelFrontBase = new ModelRenderer(this, 0, 0);
+		this.crownJewelFrontBase.setPos(0.0F, -9.2F, -0.5F);
+		this.crownJewelFrontBase.addBox(-1.0F, 0.0F, -1.0F, 2, 2, 1, 0.0F);
+		this.crownBorderRight = new ModelRenderer(this, 18, 24);
+		this.crownBorderRight.setPos(-3.0F, 1.3F, 0.0F);
+		this.crownBorderRight.addBox(-1.0F, -9.0F, -3.0F, 1, 2, 6, 0.0F);
+		this.crownUpper = new ModelRenderer(this, 0, 0);
+		this.crownUpper.setPos(0.0F, -2.7F, 0.0F);
+		this.crownUpper.addBox(-4.0F, -7.0F, -4.0F, 8, 2, 8, 0.0F);
+		this.crownJewelFront = new ModelRenderer(this, 0, 3);
+		this.crownJewelFront.setPos(0.0F, 0.0F, -0.7F);
+		this.crownJewelFront.addBox(-0.5F, 0.5F, -0.5F, 1, 1, 1, 0.0F);
+		this.crownBorderBack = new ModelRenderer(this, 0, 24);
+		this.crownBorderBack.setPos(0.0F, 1.3F, 2.0F);
+		this.crownBorderBack.addBox(-4.0F, -9.0F, 1.0F, 8, 2, 1, 0.0F);
+		this.head = new ModelRenderer(this, 0, 10);
+		this.head.setPos(0.0F, 0.0F, 0.0F);
+		this.head.addBox(-3.0F, -7.0F, -3.0F, 6, 1, 6, 0.0F);
+		this.crownBorderFront = new ModelRenderer(this, 0, 29);
+		this.crownBorderFront.setPos(0.0F, 1.3F, -3.0F);
+		this.crownBorderFront.addBox(-4.0F, -9.0F, -1.0F, 8, 2, 1, 0.0F);
+
+		this.head.addChild(this.crownBorderLeft);
+		this.crownBorderFront.addChild(this.crownJewelFrontBase);
+		this.head.addChild(this.crownBorderRight);
+		this.head.addChild(this.crownUpper);
+		this.crownJewelFrontBase.addChild(this.crownJewelFront);
+		this.head.addChild(this.crownBorderBack);
+		this.head.addChild(this.crownBorderFront);
+		this.hat.visible = true;
+		this.body.visible = true;
+		this.leftArm.visible = true;
+		this.rightArm.visible = true;
+		this.leftLeg.visible = true;
+		this.rightLeg.visible = true;
+	}
+
+	//Not needed?
+	/*@Override
+	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		float f = 1.3F;
+		float f1 = f - 1.0F;
+
+		this.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, entityIn);
+		GlStateManager.pushMatrix();
+
+		if (this.isChild) {
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			GlStateManager.translate(0.0F, 16.0F * scale, 0.0F);
+		} else if (entityIn.isSneaking()) {
+			GlStateManager.translate(0.0F, 0.2F, 0.0F);
+		}
+		GlStateManager.translate(-this.head.rotationPointX * 0.0625F * f1, -this.head.rotationPointY * 0.0625F * f1, -this.head.rotationPointZ * 0.0625F * f1);
+		GlStateManager.scale(f, f, f);
+		this.head.render(scale);
+
+		GlStateManager.popMatrix();
+	}*/
+	
+	
+	/*@Override
+	public void renderToBuffer(MatrixStack pMatrixStack, IVertexBuilder pBuffer, int pPackedLight, int pPackedOverlay, float pRed, float pGreen, float pBlue, float pAlpha) {
+		super.renderToBuffer(pMatrixStack, pBuffer, pPackedLight, pPackedOverlay, pRed, pGreen, pBlue, pAlpha);
+		
+		float f = 1.3F;
+		float f1 = f - 1.0F;
+
+		this.applyRotations(this.head, model.bipedHead);
+		GlStateManager.pushMatrix();
+
+		if (this.young) {
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			GlStateManager.translate(0.0F, 16.0F * scale, 0.0F);
+		} else if (this.crouching) {
+			GlStateManager.translate(0.0F, 0.2F, 0.0F);
+		}
+		if (renderer instanceof RenderCQREntity) {
+			((RenderCQREntity<?>) renderer).setupHeadOffsets(this.head, EquipmentSlotType.HEAD);
+		}
+		GlStateManager.translate(-this.head.rotationPointX * 0.0625F * f1, -this.head.rotationPointY * 0.0625F * f1, -this.head.rotationPointZ * 0.0625F * f1);
+		GlStateManager.scale(f, f, f);
+		this.head.render(scale);
+
+		GlStateManager.popMatrix();
+		this.resetRotations(this.head);
+	}*/
+
+	/*@Override
+	public void render(Entity entityIn, float scale, RenderCQREntity<?> renderer, ModelBiped model, EquipmentSlotType slot) {
+		this.render(entityIn, scale, renderer, model);
+	}
+
+	public void render(Entity entityIn, float scale, EntityRenderer<?> renderer, ModelBiped model) {
+		float f = 1.3F;
+		float f1 = f - 1.0F;
+
+		this.applyRotations(this.head, model.bipedHead);
+		GlStateManager.pushMatrix();
+
+		if (this.isChild) {
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			GlStateManager.translate(0.0F, 16.0F * scale, 0.0F);
+		} else if (entityIn.isSneaking()) {
+			GlStateManager.translate(0.0F, 0.2F, 0.0F);
+		}
+		if (renderer instanceof RenderCQREntity) {
+			((RenderCQREntity<?>) renderer).setupHeadOffsets(this.head, EquipmentSlotType.HEAD);
+		}
+		GlStateManager.translate(-this.head.rotationPointX * 0.0625F * f1, -this.head.rotationPointY * 0.0625F * f1, -this.head.rotationPointZ * 0.0625F * f1);
+		GlStateManager.scale(f, f, f);
+		this.head.render(scale);
+
+		GlStateManager.popMatrix();
+		this.resetRotations(this.head);
+	}*/
+
+}
---
+++ src/main/java/team/cqr/cqweaponry/client/model/armor/ModelCustomArmorBase.java
@@ -0,0 +1,304 @@
+package team.cqr.cqweaponry.client.model.armor;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ArmorStandEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ModelCustomArmorBase<T extends LivingEntity> extends BipedModel<T> {
+
+	//private final FloatPriorityQueue rotations = new FloatArrayFIFOQueue();
+
+	public ModelCustomArmorBase(float scale, int textureWidth, int textureHeight) {
+		super(scale, 0.0F, textureWidth, textureHeight);
+	}
+
+	@Override
+	public void setupAnim(T pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch) {
+		super.setupAnim(pEntity, pLimbSwing, pLimbSwingAmount, pAgeInTicks, pNetHeadYaw, pHeadPitch);
+
+		if (pEntity instanceof ArmorStandEntity) {
+			ArmorStandEntity entityarmorstand = (ArmorStandEntity) pEntity;
+			this.head.xRot = 0.017453292F * entityarmorstand.getHeadPose().getX();
+			this.head.yRot = 0.017453292F * entityarmorstand.getHeadPose().getY();
+			this.head.zRot = 0.017453292F * entityarmorstand.getHeadPose().getZ();
+			this.head.setPos(0.0F, 1.0F, 0.0F);
+			this.body.xRot = 0.017453292F * entityarmorstand.getBodyPose().getX();
+			this.body.yRot = 0.017453292F * entityarmorstand.getBodyPose().getY();
+			this.body.zRot = 0.017453292F * entityarmorstand.getBodyPose().getZ();
+			this.leftArm.xRot = 0.017453292F * entityarmorstand.getLeftArmPose().getX();
+			this.leftArm.yRot = 0.017453292F * entityarmorstand.getLeftArmPose().getY();
+			this.leftArm.zRot = 0.017453292F * entityarmorstand.getLeftArmPose().getZ();
+			this.rightArm.xRot = 0.017453292F * entityarmorstand.getRightArmPose().getX();
+			this.rightArm.yRot = 0.017453292F * entityarmorstand.getRightArmPose().getY();
+			this.rightArm.zRot = 0.017453292F * entityarmorstand.getRightArmPose().getZ();
+			this.leftLeg.xRot = 0.017453292F * entityarmorstand.getLeftLegPose().getX();
+			this.leftLeg.yRot = 0.017453292F * entityarmorstand.getLeftLegPose().getY();
+			this.leftLeg.zRot = 0.017453292F * entityarmorstand.getLeftLegPose().getZ();
+			this.leftLeg.setPos(1.9F, 11.0F, 0.0F);
+			this.rightLeg.xRot = 0.017453292F * entityarmorstand.getRightLegPose().getX();
+			this.rightLeg.yRot = 0.017453292F * entityarmorstand.getRightLegPose().getY();
+			this.rightLeg.zRot = 0.017453292F * entityarmorstand.getRightLegPose().getZ();
+			this.rightLeg.setPos(-1.9F, 11.0F, 0.0F);
+
+			this.rightArm.copyFrom(this.head);
+		}
+	}
+
+	public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.xRot = x;
+		modelRenderer.yRot = y;
+		modelRenderer.zRot = z;
+	}
+	
+	/*@Override
+	public void renderToBuffer(MatrixStack pMatrixStack, IVertexBuilder pBuffer, int pPackedLight, int pPackedOverlay, float pRed, float pGreen, float pBlue, float pAlpha) {
+		this.applyRotations(model);
+
+		pMatrixStack.pushPose();
+
+		if (this.young) {
+			pMatrixStack.scale(0.75F, 0.75F, 0.75F);
+			pMatrixStack.translate(0.0F, 16.0F * scale, 0.0F);
+
+			pMatrixStack.pushPose();
+			renderer.setupHeadOffsets(this.head, slot);
+			this.head.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.popPose();
+			pMatrixStack.pushPose();
+			pMatrixStack.scale(0.5F, 0.5F, 0.5F);
+			pMatrixStack.translate(0.0F, 24.0F * scale, 0.0F);
+
+			pMatrixStack.pushPose();
+			renderer.setupBodyOffsets(this.body, slot);
+			this.body.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupRightArmOffsets(this.rightArm, slot);
+			this.rightArm.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupLeftArmOffsets(this.leftArm, slot);
+			this.leftArm.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupRightLegOffsets(this.rightLeg, slot);
+			this.rightLeg.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupLeftLegOffsets(this.leftLeg, slot);
+			this.leftLeg.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupHeadwearOffsets(this.hat, slot);
+			this.hat.render(scale);
+			pMatrixStack.popPose();
+		} else {
+			if (entityIn.isCrouching()) {
+				pMatrixStack.translate(0.0F, 0.2F, 0.0F);
+			}
+
+			pMatrixStack.pushPose();
+			renderer.setupHeadOffsets(this.head, slot);
+			this.head.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupBodyOffsets(this.body, slot);
+			this.body.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupRightArmOffsets(this.rightArm, slot);
+			this.rightArm.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupLeftArmOffsets(this.leftArm, slot);
+			this.leftArm.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupRightLegOffsets(this.rightLeg, slot);
+			this.rightLeg.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupLeftLegOffsets(this.leftLeg, slot);
+			this.leftLeg.render(scale);
+			pMatrixStack.popPose();
+
+			pMatrixStack.pushPose();
+			renderer.setupHeadwearOffsets(this.hat, slot);
+			this.hat.render(scale);
+			pMatrixStack.popPose();
+		}
+
+		pMatrixStack.popPose();
+
+		this.resetRotations();
+	}
+	
+	// TODO: Exchange type of "model" to something like "IBipedArmorPoseProvider", this one has methods to set the rotations
+	// of the armor bones => interface that
+	// has to be implemented by the model!
+	// TODO: Move the "setupXOffsets" methods to an interface that needs to be implemented by the renderer
+	public void render(T entityIn, float scale, RenderCQREntity<?> renderer, ModelBiped model, EntityEquipmentSlot slot) {
+		this.applyRotations(model);
+
+		GlStateManager.pushPose();
+
+		if (this.young) {
+			GlStateManager.scale(0.75F, 0.75F, 0.75F);
+			GlStateManager.translate(0.0F, 16.0F * scale, 0.0F);
+
+			GlStateManager.pushPose();
+			renderer.setupHeadOffsets(this.head, slot);
+			this.head.render(scale);
+			GlStateManager.popPose();
+
+			GlStateManager.popPose();
+			GlStateManager.pushPose();
+			GlStateManager.scale(0.5F, 0.5F, 0.5F);
+			GlStateManager.translate(0.0F, 24.0F * scale, 0.0F);
+
+			GlStateManager.pushPose();
+			renderer.setupBodyOffsets(this.body, slot);
+			this.body.render(scale);
+			GlStateManager.popPose();
+
+			GlStateManager.pushPose();
+			renderer.setupRightArmOffsets(this.rightArm, slot);
+			this.rightArm.render(scale);
+			GlStateManager.popPose();
+
+			GlStateManager.pushPose();
+			renderer.setupLeftArmOffsets(this.leftArm, slot);
+			this.leftArm.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushPose();
+			renderer.setupRightLegOffsets(this.rightLeg, slot);
+			this.rightLeg.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushPose();
+			renderer.setupLeftLegOffsets(this.leftLeg, slot);
+			this.leftLeg.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushPose();
+			renderer.setupHeadwearOffsets(this.hat, slot);
+			this.hat.render(scale);
+			GlStateManager.popMatrix();
+		} else {
+			if (entityIn.isCrouching()) {
+				GlStateManager.translate(0.0F, 0.2F, 0.0F);
+			}
+
+			GlStateManager.pushPose();
+			renderer.setupHeadOffsets(this.head, slot);
+			this.head.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushPose();
+			renderer.setupBodyOffsets(this.body, slot);
+			this.body.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushMatrix();
+			renderer.setupRightArmOffsets(this.rightArm, slot);
+			this.rightArm.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushMatrix();
+			renderer.setupLeftArmOffsets(this.leftArm, slot);
+			this.leftArm.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushMatrix();
+			renderer.setupRightLegOffsets(this.rightLeg, slot);
+			this.rightLeg.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushMatrix();
+			renderer.setupLeftLegOffsets(this.leftLeg, slot);
+			this.leftLeg.render(scale);
+			GlStateManager.popMatrix();
+
+			GlStateManager.pushMatrix();
+			renderer.setupHeadwearOffsets(this.hat, slot);
+			this.hat.render(scale);
+			GlStateManager.popMatrix();
+		}
+
+		GlStateManager.popMatrix();
+
+		this.resetRotations();
+	}
+
+	public void applyRotations(BipedModel<?> model) {
+		this.applyRotations(this.hat, model.hat);
+		this.applyRotations(this.head, model.head);
+		this.applyRotations(this.body, model.body);
+		this.applyRotations(this.leftArm, model.leftArm);
+		this.applyRotations(this.rightArm, model.rightArm);
+		this.applyRotations(this.leftLeg, model.leftLeg);
+		this.applyRotations(this.rightLeg, model.rightLeg);
+	}
+
+	public void applyRotations(ModelRenderer target, ModelRenderer source) {
+		this.rotations.enqueue(target.offsetX);
+		this.rotations.enqueue(target.offsetY);
+		this.rotations.enqueue(target.offsetZ);
+		this.rotations.enqueue(target.xRot);
+		this.rotations.enqueue(target.yRot);
+		this.rotations.enqueue(target.zRot);
+		this.rotations.enqueue(target.rotationPointX);
+		this.rotations.enqueue(target.rotationPointY);
+		this.rotations.enqueue(target.rotationPointZ);
+
+		target.offsetX = source.offsetX;
+		target.offsetY = source.offsetY;
+		target.offsetZ = source.offsetZ;
+		target.xRot = source.xRot;
+		target.yRot = source.yRot;
+		target.rotateAngleZ = source.rotateAngleZ;
+		target.rotationPointX = source.rotationPointX;
+		target.rotationPointY = source.rotationPointY;
+		target.rotationPointZ = source.rotationPointZ;
+	}
+
+	public void resetRotations() {
+		this.resetRotations(this.hat);
+		this.resetRotations(this.head);
+		this.resetRotations(this.body);
+		this.resetRotations(this.leftArm);
+		this.resetRotations(this.rightArm);
+		this.resetRotations(this.leftLeg);
+		this.resetRotations(this.rightLeg);
+	}
+
+	public void resetRotations(ModelRenderer target) {
+		target.offsetX = this.rotations.dequeue();
+		target.offsetY = this.rotations.dequeue();
+		target.offsetZ = this.rotations.dequeue();
+		target.rotateAngleX = this.rotations.dequeue();
+		target.rotateAngleY = this.rotations.dequeue();
+		target.rotateAngleZ = this.rotations.dequeue();
+		target.rotationPointX = this.rotations.dequeue();
+		target.rotationPointY = this.rotations.dequeue();
+		target.rotationPointZ = this.rotations.dequeue();
+	}*/
+
+}
